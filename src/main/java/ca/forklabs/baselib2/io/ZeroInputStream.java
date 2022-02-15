6d5b08e350b02d042de5f078f8ed41b84502d488
Daniel Léonard <leonard.daniel@gmail.com>
2022-02-14 23:35:31 -0500

    Null, random, and constant streams

--- src/main/java/ca/forklabs/baselib2/io/ZeroInputStream.java
+++ src/main/java/ca/forklabs/baselib2/io/ZeroInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)  2006  Daniel Léonard
+ * Copyright (C)  2006  Forklabs Daniel Léonard
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
--- src/test/java/ca/forklabs/baselib2/io/ConstantInputStreamTest.java
+++ src/test/java/ca/forklabs/baselib2/io/ConstantInputStreamTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011  Daniel Léonard
+ * Copyright (C) 2011  Forklabs Daniel Léonard
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
